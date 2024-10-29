9ae449ec2adbba3ab431c887c435d44f4c28c508
Manases Lovera <manaseslovera07@gmail.com>
2024-10-29 05:59:05 -0400

    feat(generic-repo): generic repository implemented with interface

--- Application/Interfaces/IGenericRepository.cs
+++ Application/Interfaces/IGenericRepository.cs
@@ -10,7 +10,7 @@ namespace Application.Interfaces
     {
         Task<IEnumerable<T>> GetAllAsync();
 
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
 
         Task AddAsync(T entity);
 
--- Infraestructure/Repositories/GenericRepository.cs
+++ Infraestructure/Repositories/GenericRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Interfaces;
+using Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +11,42 @@ namespace Infraestructure.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public async Task AddAsync(T entity)
         {
-            throw new NotImplementedException();
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Set<T>().ToListAsync();
         }
 
-        public async Task<T> GetByIdAsync(int id)
+        public async Task<T?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Set<T>().FindAsync(id);
         }
 
         public async Task UpdateAsync(int id, T entity)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
