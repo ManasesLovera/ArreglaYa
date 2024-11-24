657f88e8d02132aa54a4d2b1d83a3c2ce84d5d13
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 17:30:33 -0400

    feat: add admin update dto

--- backend/Application/DTOs/Admin/UpdateAdminDTos.cs
+++ backend/Application/DTOs/Admin/UpdateAdminDTos.cs
@@ -10,8 +10,6 @@ namespace Application.DTOs.Admin
     {
         public string? Email { get; set; }
 
-        public string? Password { get; set; }
-
         public string? Phone { get; set; }
     }
 }
