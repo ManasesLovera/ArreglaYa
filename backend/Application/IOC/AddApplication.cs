dc541b7b44a4d383dfa1ac2e16f82202ee842166
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 17:41:22 -0400

    buid: add extension method application layer in container dependency

---
+++ backend/Application/IOC/AddApplication.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces.Services;
+using Application.Mapper;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IOC
+{
+    public static class AddApplication
+    {
+        public static void AddAplicationLayer(this IServiceCollection services)
+        {
+            #region Mapper
+            services.AddAutoMapper(typeof(MapperProfile));
+            #endregion
+
+            #region Admin
+            services.AddScoped<IAdminService, AdminService>();
+            #endregion
+        }
+    }
+}
