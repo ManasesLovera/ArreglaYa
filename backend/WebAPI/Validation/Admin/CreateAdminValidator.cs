72c9c6cda450f1db6a8e33ecd027284d8c379649
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 17:49:23 -0400

    feat: add admin validation

---
+++ backend/WebAPI/Validation/Admin/CreateAdminValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Admin;
+using FluentValidation;
+
+namespace WebAPI.Validation.Admin
+{
+    public class CreateAdminValidator : AbstractValidator<RegisterRequest>
+    {
+        public CreateAdminValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress().WithMessage("It is not an email address");
+            RuleFor(x => x.Email).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Username).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Password).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Password).MinimumLength(5).WithMessage("The minimum number of characters is {MinLength}");
+            RuleFor(x => x.Phone).NotNull().WithMessage("Is required {PropertyName}");
+            RuleFor(x => x.Phone).MinimumLength(5).WithMessage("The minimum number of characters is {MinLength}");
+
+        }
+    }
+}
