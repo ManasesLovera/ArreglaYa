1799b89677961e8cdd73f23221124a35aa2fa556
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 14:32:55 -0400

    feat: add admin dtos

---
+++ backend/Application/DTOs/Admin/RegisterRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin
+{
+    public class RegisterRequest
+    {
+        public string? Email { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public string? Phone {  get; set; }
+    }
+}
