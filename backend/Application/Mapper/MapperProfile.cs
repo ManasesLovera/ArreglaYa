1d4ddb69d7094a559aea85dfcaef184d6fa12974
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 14:23:51 -0400

    feat: add mapper admin

---
+++ backend/Application/Mapper/MapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs.Admin;
+using AutoMapper;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            #region Admin
+            CreateMap<Admin, UpdateAdminDTos>()
+                            .ReverseMap();
+            CreateMap<Admin, AdminDTos>()
+                            .ReverseMap(); 
+            #endregion
+        }
+
+    }
+}
