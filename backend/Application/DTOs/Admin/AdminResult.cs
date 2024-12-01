714c36db9c44e47fb0afcd8bf03dc6859dcdf1d4
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-30 20:42:56 -0400

    feat: add admin result record

---
+++ backend/Application/DTOs/Admin/AdminResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Admin
+{
+    public record AdminResult
+    (
+        bool IsSuccessful,
+        Domain.Models.Admin Admin,
+        string Message
+    );
+}
