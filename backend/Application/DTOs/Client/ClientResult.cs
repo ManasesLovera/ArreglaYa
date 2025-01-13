5f448a278010d15201561a21811b62f2d43784e1
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-13 18:40:33 -0400

    feature: add client dto and client result

---
+++ backend/Application/DTOs/Client/ClientResult.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Client
+{
+    public record ClientResult
+    (
+        bool IsSuccessful,
+        ClientDto Client,
+        string Message
+    );
+}
