fc7916f42fdf39425347b774991cad40cb4118a7
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-13 18:41:20 -0400

    feature: add validation for create client

---
+++ backend/WebAPI/Validation/Client/CreateClientValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.Admin;
+using FluentValidation;
+
+namespace WebAPI.Validation.Client
+{
+    public class CreateClientValidator : AbstractValidator<RegisterRequest>
+    {
+        public CreateClientValidator()
+        {
+
+            RuleFor(x => x.Email).EmailAddress().WithMessage("It is not an email address");
+            RuleFor(x => x.Email).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Username).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Password).NotNull().WithMessage("Can't not null {PropertyName}");
+            RuleFor(x => x.Password).MinimumLength(5).WithMessage("The minimum number of characters is {MinLength}");
+
+        }
+    }
+}
