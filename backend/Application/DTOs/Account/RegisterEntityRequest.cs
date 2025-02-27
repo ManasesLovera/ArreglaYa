d524e62e988849551244cb2bc0dcd16359836070
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-27 12:18:26 -0400

    feat: add account dtos

---
+++ backend/Application/DTOs/Account/RegisterEntityRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Account
+{
+    public record RegisterEntityRequest
+    (
+        string Email,
+        string Username,
+        string Password,
+        string FullName
+    );
+}
